de2b4ad3bb47df05dec0eb2b3a469a085e9c824c
Eckra Wangui <+>
2023-11-23 14:07:47 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while counter>0:
+        print(counter)
+        counter -=1
+        print ("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_elemnts= [i ** 2 for i in int_list]
+    return squared_elemnts
+
+    pass 
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3==0 and num % 5==0:
+           print("FizzBuzz")
+        elif num % 3==0:
+           print("Fizz")
+        elif num % 5==0:
+           print("Buzz")
+        else:
+           print(num)
+ 
